4c166ac5d4a85a2a2708f154fce97132f2e992af
shakthi2007 <63248034+shakthi2007@users.noreply.github.com>
2020-05-18 20:57:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(800,400);
+}
+
+function draw() {
+ background("black"); 
+ triangle (100,50,70,100,130,100); 
+ rect (70,100,60,400);
+ rect (128,200,600,200);
+ rect (700,100,60,400);
+ triangle (700,100,760,100,730,40);
+
+} 
